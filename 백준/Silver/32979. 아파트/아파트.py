55f4a5a0f845d0c9f11788308f9705833139bcf3
jisooOh <122949955+jsoo5@users.noreply.github.com>
2025-03-09 15:58:37 +0900

    [Silver V] Title: 아파트, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/32979. 아파트/아파트.py
+++ 백준/Silver/32979. 아파트/아파트.py
@@ -7,13 +7,8 @@ a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 loser = []
 
-def game_start(apart, num):
-    i = 1
-    while i < num:
-        apart.append(apart.pop(0))
-        i += 1
-    return apart[0]
-
+i = 0
 for j in range(T):
-    loser.append(game_start(a, b[j]))
+    i = (i + b[j] - 1) % len(a)
+    loser.append(a[i])
 print(*loser, sep=' ')
