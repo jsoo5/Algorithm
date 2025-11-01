c23e9d266175f2f0887ee62ebafc52a008eecca9
jisooOh <122949955+jsoo5@users.noreply.github.com>
2025-11-01 18:14:50 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,13 +1,14 @@
 def solution(dots):
     
     from itertools import combinations
-    comb = []
     
-    for i in combinations(combinations(dots, 2), 2):
-        if not ((i[0][0] in i[1]) or (i[0][1] in i[1])):
-            x1, y1, x2, y2 = i[0][0][0], i[0][0][1], i[0][1][0], i[0][1][1]
-            x3, y3, x4, y4 = i[1][0][0], i[1][0][1], i[1][1][0], i[1][1][1]
-            print(i)
+    for a, b in combinations(combinations(dots, 2), 2):
+        if not ((a[0] in b) or (a[1] in b)):
+
+            x1, y1 = a[0]
+            x2, y2 = a[1]
+            x3, y3 = b[0]
+            x4, y4 = b[1]
 
             if ((y2 - y1) / (x2 - x1)) == ((y4 - y3) / (x4 - x3)):
                 return 1
